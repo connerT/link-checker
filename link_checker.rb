3ea2daf97b57918a43d42ee61ee2eab87f3c598f
Conner Tolley <connertolley@gmail.com>
2012-02-12 15:55:18 -0600

    added error log to external file

--- link_checker.rb
+++ link_checker.rb
@@ -5,7 +5,7 @@ require 'uri'
 
 b = Watir::Browser.new :firefox, :profile => 'WebDriver'   # This is my custom firefox profile for webdriver
 b.driver.manage.timeouts.implicit_wait = 3 #3 seconds
-b.goto 'https://www.vitalchek.com/express-birth-certificates.aspx'
+b.goto 'github.com'
 
 mylinks = b.links.collect
 puts "Number of links is #{mylinks.size}"
@@ -18,9 +18,9 @@ mylinks.each do |link|
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
   puts "#{link.href} is #{response.code}"
-  #unless response.code == '200'
-    #File.open('error_log.txt','a+'){|file| file.puts "#{link.href} is #{response.code}" } #add a date timestamp
-  #end
+  unless response.code == '200'
+    File.open('error_log.txt','a+'){|file| file.puts "#{link.href} is #{response.code}" } #add a date timestamp
+  end
 end
 
 b.close
